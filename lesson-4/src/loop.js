289118574ef0eb17aaa747ec4b10427dab8e3018
Ramella Basenko <ramellabasenko@gmail.com>
2025-10-05 18:46:25 +0200

    added while and do while loops

--- lesson-4/src/loop.js
+++ lesson-4/src/loop.js
@@ -6,3 +6,29 @@ console.log('Цикл від 100 до 0 з кроком 10:');
 for (let j = 100; j >= 0; j -= 10) {
     console.log(j);
 }
+//додаток з while 
+console.log("Цикл while від 0 до 9:");
+let a = 0;
+while (a <= 9) {
+  console.log(a);
+  a++;
+}
+console.log("Цикл while від 100 до 0 з кроком 10:");
+let b = 100;
+while (b >= 0) {
+  console.log(b);
+  b -= 10;
+}
+// додаток do...while  
+console.log("Цикл do...while від 0 до 9:");
+let x = 0;
+do {
+  console.log(x);
+  x++;
+} while (x <= 9);
+console.log("Цикл do...while від 100 до 0 з кроком 10:");
+let y = 100;
+do {
+  console.log(y);
+  y -= 10;
+} while (y >= 0);
